a45ca29e0b33525ebd8f6506929b45fa1a859a87
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-03 00:11:18 +0530

    added some style to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Box , Button} from '@chakra-ui/react'
 function Navbar(){
     const {isAuth, toggleAuth} = useContext(AuthContext);
     return(
-        <Box>
+        <Box display='flex' flexDirection='row-reverse' backgroundColor='green' padding='6' borderRadius='2xl'>
             <Button onClick={()=>(toggleAuth(isAuth?false:true))}>
                 {isAuth?"Logout":"Login"}
             </Button>
