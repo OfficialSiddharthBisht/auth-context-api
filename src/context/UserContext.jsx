bca17fe1c81e4622387593b294efc8825f04a7dd
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-03 00:01:57 +0530

    added code to userContext file

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,1 +1,12 @@
-i
+import React , {createContext} from 'react';
+
+export const AuthContext = createContext();
+
+function AuthContextProvider({children}){
+    const [isAuth, toggleAuth] = React.useState(false);
+    return(
+        <AuthContext.Provider value = {{isAuth,toggleAuth}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
