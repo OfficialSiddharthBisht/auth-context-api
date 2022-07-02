4cad877741c7512c6bbffcecf7ee53ed919af910
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-03 00:08:18 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React , {useContext} from "react";
+import { AuthContext } from "../context/UserContext";
+import {Box , Button} from '@chakra-ui/react'
+
+function Navbar(){
+    const {isAuth, toggleAuth} = useContext(AuthContext);
+    return(
+        <Box>
+            <Button onClick={()=>(toggleAuth(isAuth?false:true))}>
+                {isAuth?"Logout":"Login"}
+            </Button>
+        </Box>
+    )
+}
+
+export default Navbar
